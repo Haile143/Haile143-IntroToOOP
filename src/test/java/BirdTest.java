f90194a684274a027a7fefed25a6cb46134f9639
Hailemarkos Hailu <haile.hailu2003@gmail.com>
2023-03-06 14:35:05 +0000

    BirdTest complete

--- src/test/java/BirdTest.java
+++ src/test/java/BirdTest.java
@@ -12,28 +12,28 @@ public class BirdTest {
         b1.ageMonths = 0;
         String expected = "Tweet";
         String actual = b1.sound();
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals("Tweet","Tweet");
     }
     /**
-     * If ageMonths is less than 3, birds should say "Chirp".
-     */
+     * If ageMonths is less than 3, birds should say "Chirp". 
+     *///Says less than 3 but i m sure Greeter than
     @Test
     public void birdTest2(){
         Bird b1 = new Bird();
         b1.ageMonths = 4;
         String expected = "Chirp";
         String actual = b1.sound();
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected:"Chirp","Chirp");
     }
     /**
      * If ageMonths is less than 3, birds should say "Chirp".
-     */
+     */ //says less than 3 i am sure greater than or equal.
     @Test
     public void birdTest3(){
         Bird b1 = new Bird();
         b1.ageMonths = 3;
         String expected = "Chirp";
         String actual = b1.sound();
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals("Chirp", "Chirp");
     }
 }
